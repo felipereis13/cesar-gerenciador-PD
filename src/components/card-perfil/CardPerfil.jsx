b30bbaaf7a8786b67bcc05940e792cb0d7da1d6c
victormilitino <victor.militino@gmail.com>
2025-05-23 09:49:41 -0300

    alteracao cardperfil, inserindo icons

--- src/components/card-perfil/CardPerfil.jsx
+++ src/components/card-perfil/CardPerfil.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import "./CardPerfil.css" // Importação do CSS
+import { CgAttachment } from "react-icons/cg";
+import { FaHistory, FaComments, FaCalendarAlt } from "react-icons/fa";
+import { IoSettingsSharp } from "react-icons/io5";
 
 export default function CardPerfil(props) {
   return (
     <div className="col-lg-3 mb-4">
       <div className="profile-card card">
         <div className="card-body">
-          <div className="profile-avatar">F</div>
+          <div className="profile-avatar">G</div>
           <div className="profile-info-card-container">
             <div className="profile-info-card">
               <p>
@@ -23,12 +26,25 @@ export default function CardPerfil(props) {
               </p>
             </div>
           </div>
-          <div className="text-center">
-            <a href="/perfil">
-              <button className="btn btn-edit-profile btn-outline-primary mt-3">
-                Editar Perfil de Usuário
-              </button>
+          <div className="icon text-center p-4">
+            <a href="/arquivo ">
+              <CgAttachment className="#" />
             </a>
+            <a href="/historico">
+              <FaHistory className="#" />
+            </a>
+            <a href="/DescricaoAtendimentos">
+              <FaComments className="#" />
+            </a>
+            <a href="/Calendario">
+              <FaCalendarAlt className="#" />
+            </a>
+            <div className="configuracao pt-5">
+              <a href="/perfil">
+              <IoSettingsSharp className="configuracao m-1"/>Editar Perfil
+            </a>
+            </div>
+
           </div>
         </div>
       </div>
