8835598fe27089543aada1b3250bd6668406c481
Felipe <felipermpires@gmail.com>
2025-05-29 16:16:45 -0300

    imagem2

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -15,7 +15,7 @@ const SideBar = () => {
     <div className="SideBar d-flex flex-column justify-content-between">
       <div>
         <div className="text-center my-4">
-          <img src="cesar.svg" alt="Logo" className="logo" />
+          <img src="/cesar.svg" alt="Logo" className="logo" />
         </div>
         <ul className="nav flex-column px-3">
           <li className="nav-item">
