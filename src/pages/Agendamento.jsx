d3cb236c5ab62a2fb9cf885337f433447626ab79
Felipe Reis <felipermpires@gmail.com>
2025-05-30 15:46:30 -0300

    ajuste agendamentos

--- src/pages/Agendamento.jsx
+++ src/pages/Agendamento.jsx
@@ -1,9 +1,18 @@
 // src/pages/Agendamento.jsx
-import React from "react"
+import React, { useRef } from "react"
 import SideBar from "../components/SideBar/SideBar"
 import "./Agendamento.css"
 
 const Agendamento = () => {
+  const dateRef = useRef(null)
+  const timeRef = useRef(null)
+
+  const handleAgendar = () => {
+    alert("Agendamento realizado com sucesso!")
+    if (dateRef.current) dateRef.current.value = ""
+    if (timeRef.current) timeRef.current.value = ""
+  }
+
   return (
     <div className="app-container d-flex">
       <SideBar />
@@ -14,13 +23,11 @@ const Agendamento = () => {
 
         <div className="agendamento-form-section">
           <p>Selecione a data e hora desejada:</p>
-          <input type="date" className="form-control" id="form-agendamento" />
-          <input
-            type="time"
-            className="form-control mt-3"
-            id="form-agendamento"
-          />
-          <button className="btn btn-primary mt-4">Agendar</button>
+          <input type="date" className="form-control" ref={dateRef} />
+          <input type="time" className="form-control mt-3" ref={timeRef} />
+          <button className="btn btn-primary mt-4" onClick={handleAgendar}>
+            Agendar
+          </button>
         </div>
       </div>
     </div>
